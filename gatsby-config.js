bd611c0802642bfab493af9f0e554a5b754ff00c
rajjeet <rajjeet.phull@gmail.com>
2019-02-16 07:25:52 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Pandas Eating Lots'
+    },
+    plugins: [
+        `gatsby-plugin-emotion`,
+        `gatsby-transformer-remark`,
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography'
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: 'src',
+                path: `${__dirname}/src`
+
+            }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'GatsbyJS',
+                short_name: "GatsbyJS",
+                start_url: '/',
+                background_color: '#6b37bf',
+                theme_color: '#6b37bf',
+                display: 'standalone',
+                icon: "src/images/icon.png"
+            }
+        },
+        `gatsby-plugin-offline`
+    ]
+};
---
+++ src/pages/pandas-and-bananas.md
@@ -0,0 +1,8 @@
+---
+title: "Pandas and Bananas"
+date: "2018-08-21"
+---
+
+Do Pandas eat bananas? Check out this short video that shows that yes! Pandas do seem to enjoy bananas.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4SZl1r2O_bY" frameborder="0" allowfullscreen></iframe>
