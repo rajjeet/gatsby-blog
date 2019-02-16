957da4f91770d691d63851c1af494bd0011abf01
rajjeet <rajjeet.phull@gmail.com>
2019-02-16 07:51:41 -0500

    Added two paragraphs to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,12 @@ import Layout from '../components/layout';
 
 export default ({data}) => (
     <Layout>
-        <h1>{data.site.siteMetadata.title}</h1>
-        <p> We're the only site running on your computer dedicated to showing the best
-            photos and videos of pandas eating lots of food.</p>
+        <h1>About Me</h1>
+        <p> Hi! I'm Raj and this is my personal blog site, Ortmesh. The purpose of my website is to share my ideas with
+            others, regarding IT topics and audibles. </p>
+        <p>
+            This website is developed using Gatsby, a static site generator powered by React & GraphQL. Hosting is done
+            on AWS S3 (storage) and CloudFront (cached content delivery) for maximum performance. </p>
     </Layout>
 )
 
