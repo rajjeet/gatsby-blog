00d68d19ebc2f95a34108d5712a450b942a89cfc
rajjeet <rajjeet.phull@gmail.com>
2019-02-16 11:03:01 -0500

    Added contact menu

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Headshot from '../images/headshot.jpg';
 import Layout from '../components/layout';
-import {Grid, Image} from "semantic-ui-react";
+import {Grid, Header, Icon, Image, Menu} from "semantic-ui-react";
 
 export default () => (
     <Layout>
-        <h1>About Me</h1>
+        <Header as={'h1'} content={'About Me'}/>
         <Grid stackable>
             <Grid.Row>
                 <Grid.Column width={11}>
@@ -20,7 +20,27 @@ export default () => (
                     </div>
                 </Grid.Column>
                 <Grid.Column width={5}>
-                    <Image centered size={'small'} src={Headshot} alt={'Headshot'} rounded />
+                    <Image  centered size={'medium'} src={Headshot} alt={'Headshot'} rounded/>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column>
+                    <Header as={'h4'} content={'Contact'}/>
+                    <Menu icon={'labeled'} borderless secondary compact stackable>
+                        <Menu.Item fitted style={{textAlign: 'left'}}>
+                            <a href={'mailto:rajjeet.phull@gmail.com?subject=Via Ortmesh:'}>
+                                <Icon link name={'mail'} size={'big'}/>Email </a>
+                        </Menu.Item>
+                        <Menu.Item fitted>
+                            <a href={'https://github.com/rajjeet'} target={'_blank'}>
+                                <Icon link name={'github'} size={'big'}/>Github </a>
+                        </Menu.Item>
+                        <Menu.Item fitted>
+                            <a href={'https://linkedin.com/in/rajjeetphull'} target={'_blank'}>
+                                <Icon link name={'linkedin'} size={'big'}/>LinkedIn </a>
+                        </Menu.Item>
+                    </Menu>
+
                 </Grid.Column>
             </Grid.Row>
         </Grid>
