96c8200f5dbf6680550649811ff3820d7cb47acc
rajjeet <rajjeet.phull@gmail.com>
2019-02-16 09:05:32 -0500

    Removed yarn lock. Made changes to imports to remove warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import Layout from '../components/layout';
+import React from 'react';
 import {Link, graphql} from "gatsby";
+import Layout from '../components/layout';
 import {css} from "@emotion/core";
 import {rhythm} from "../utils/typography";
 
