be826e1a1e2b4ac746fe39b47637edc2a66032ec
rajjeet <rajjeet.phull@gmail.com>
2019-02-16 08:46:53 -0500

    Added a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default () => <Layout>
+    <h4>Error 404:</h4>
+    <p> This page is not found. Please enter a valid URL path into the address bar.</p>
+    <a href={'/'}>Go to Home</a>
+</Layout>;
