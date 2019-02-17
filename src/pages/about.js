12e4636db92d39cb1dc2f04e89bdda6f572460bc
rajjeet <rajjeet.phull@gmail.com>
2019-02-17 14:13:10 -0500

    Fixed up the about page content.

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,13 +10,15 @@ export default () => (
             <Grid.Row>
                 <Grid.Column width={11}>
                     <div>
-                        <p> Hi! I'm Raj and this is my personal blog site, Ortmesh! The purpose of my website is to
-                            share my
-                            ideas with others, regarding IT topics and audibles. </p>
-                        <p>
-                            This website is developed using Gatsby, a static site generator powered by React & GraphQL.
-                            Hosting
-                            is done on AWS S3 Storage and CloudFront (CDN) for maximum performance. </p>
+                        <p>I'm Raj and welcome to my personal blog site, Ortmesh! The purpose of my website is to
+                            share my ideas with others, regarding IT topics and audio books. </p>
+                        <p>For the past three years, I've been a full stack developer at TD Canada Trust in Toronto,
+                            using React, .NET Framework (C#) and SQL Server to help develop an internal web application.
+                            Data warehousing, reporting, and data analytics are also my core areas of expertise. My
+                            current certifications are Certified AWS Solutions Architect and Microsoft Certified
+                            Professional in Data Management and Analytics. </p>
+                        <p>I work on side projects outside of work. I will be updating my portfolio on this website to
+                            showcase some of those projects.</p>
                     </div>
                 </Grid.Column>
                 <Grid.Column width={5}>
@@ -25,17 +27,17 @@ export default () => (
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column>
-                    <Header as={'h4'} content={'Contact'}/>
-                    <Grid columns={3} stackable>
-                        <Grid.Column floated={'left'}>
+                    <Header as={'h4'} content={'Links'}/>
+                    <Grid stackable columns={3}>
+                        <Grid.Column width={2}>
                             <a href={'mailto:rajjeet.phull@gmail.com?subject=Via Ortmesh:'}>
                                 <Icon link name={'mail'} size={'big'}/>Email </a>
                         </Grid.Column>
-                        <Grid.Column floated={'left'}>
+                        <Grid.Column width={2}>
                             <a href={'https://github.com/rajjeet'} target={'_blank'}>
                                 <Icon link name={'github'} size={'big'}/>Github </a>
                         </Grid.Column>
-                        <Grid.Column floated={'left'}>
+                        <Grid.Column width={2}>
                             <a href={'https://linkedin.com/in/rajjeetphull'} target={'_blank'}>
                                 <Icon link name={'linkedin'} size={'big'}/>LinkedIn </a>
                         </Grid.Column>
