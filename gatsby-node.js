8af44ad5c03572caa01547df478709e27433554d
rajjeet <rajjeet.phull@gmail.com>
2019-02-21 08:01:38 -0500

    Serving posts from posts folder instead of pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,7 @@ exports.onCreateNode = ({node, getNode, actions}) => {
     const {createNodeField} = actions;
 
     if (node.internal.type === `MarkdownRemark`) {
-        const slug = createFilePath({node, getNode, basePath: `pages`});
+        const slug = createFilePath({node, getNode, basePath: `pages/posts`});
         createNodeField({
             node,
             name: `slug`,
