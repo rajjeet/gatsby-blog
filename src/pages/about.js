86c34354939827cb949220a16e70e9f2eb6764a3
phullr2-at-224947627700 <rajjeet.phull@gmail.com>
2019-04-21 14:41:40 -0400

    Updated about me section

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,25 +7,39 @@ import SEO from "../components/SEO";
 
 export default () => (
     <Layout>
-        <SEO />
+        <SEO/>
         <Header as={'h1'} content={'About Me'}/>
         <Grid stackable>
             <Grid.Row>
                 <Grid.Column width={11}>
                     <div>
-                        <p>I'm Raj and welcome to my personal blog site, Ortmesh! The purpose of my website is to
-                            share my ideas with others. </p>
-                        <p>For the past three years, I've been a full stack developer at TD Canada Trust in Toronto,
-                            using React, .NET Framework (C#) and SQL Server to help develop an internal web application.
-                            Data warehousing, reporting, and data analytics are also my core areas of expertise. My
-                            current certifications are Certified AWS Solutions Architect and Microsoft Certified
-                            Professional in Data Management and Analytics. </p>
-                        <p>I work on side projects outside of work. I will be updating my portfolio on this website to
-                            showcase some of those projects.</p>
+                        <p>
+                            I'm Raj and welcome to my personal blog site, Ortmesh! The purpose of my website is to
+                            share my ideas with others and connect.
+                        </p>
+                        <h3>Work Experience</h3>
+                        <p>
+                            For the past few years, I've been a full stack web developer at TD Canada Trust in Toronto.
+                            My areas of specialties are in React JS web apps, SQL Server databases, .NET APIs, and AWS
+                            design and automation.
+                            At work, I'm focused on delivering high quality features for an internal project that
+                            involves
+                            data warehouse automation, reporting, data analytics/visualization, and web applications.
+                        </p>
+                        <p>
+                            My current certifications are Certified AWS Solutions Architect and Microsoft Certified
+                            Professional in Data Management and Analytics.
+                        </p>
+                        <h3>Outside of Work</h3>
+                        <p>
+                            Other than spending time with family, I'm always coding side projects or writing blogs.
+                            I'm also looking for ways to help others with my knowledge and skills in software development and
+                            cloud architecture.
+                        </p>
                     </div>
                 </Grid.Column>
                 <Grid.Column width={5}>
-                    <Headshot />
+                    <Headshot/>
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
