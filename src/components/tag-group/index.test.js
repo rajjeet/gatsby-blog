538a3c3c80c3ceaab245a275ef43b26fd74f06e2
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-08-31 06:27:05 -0400

    add test for tag group

--- src/components/tag-group/index.test.js
+++ src/components/tag-group/index.test.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, cleanup } from '@testing-library/react';
 import TagGroup from './index';
 import { makeProps } from './mock';
 
+afterEach(cleanup);
+
 describe('<TagGroup />', () => {
   it('should render', () => {
-    renderer.create(<TagGroup {...makeProps()} />);
+    const { asFragment } = render(<TagGroup {...makeProps()} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should link for each tag group', () => {
+    const { queryAllByRole } = render(<TagGroup {...makeProps()} />);
+    expect(queryAllByRole('link')).toHaveLength(2);
+  });
+
+  it('should the name of each tag group', () => {
+    const { getByText } = render(<TagGroup {...makeProps()} />);
+    expect(getByText('Experience')).toBeDefined();
+    expect(getByText('Guide')).toBeDefined();
+  });
+
+  it('should the count of each tag group', () => {
+    const { getByText } = render(<TagGroup {...makeProps()} />);
+    expect(getByText('1')).toBeDefined();
+    expect(getByText('2')).toBeDefined();
   });
 });
