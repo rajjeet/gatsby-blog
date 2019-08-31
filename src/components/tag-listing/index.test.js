83a2d05cde2b458643fdf07ba5f55a1a7ee5dddc
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-08-31 06:32:37 -0400

    add test for tag listing

--- src/components/tag-listing/index.test.js
+++ src/components/tag-listing/index.test.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, cleanup } from '@testing-library/react';
 import { StaticQuery } from 'gatsby';
 import TagListing from './index';
 import { createMockGroups } from '../../utils/testing';
 import { makeProps } from './mock';
 
+beforeEach(cleanup);
+
 beforeEach(() => {
-  StaticQuery.mockImplementationOnce(({ render }) => render(
+  StaticQuery.mockImplementationOnce(({ render: renderQuery }) => renderQuery(
     createMockGroups,
   ));
 });
 
 describe('<TagListing />', () => {
   it('should render', () => {
-    renderer.create(<TagListing {...makeProps()} />);
+    const { asFragment } = render(<TagListing {...makeProps()} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should have a heading', () => {
+    const { getByText } = render(<TagListing {...makeProps()} />);
+    expect(getByText('Tags')).toBeDefined();
+  });
+
+  it('should the name of each tag group', () => {
+    const { getByText } = render(<TagListing {...makeProps()} />);
+    expect(getByText('Experience')).toBeDefined();
+    expect(getByText('Guide')).toBeDefined();
   });
 });
