48ccaf6643bf74f1357d3cefc4e1a9b16846ad71
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-08-31 10:11:34 -0400

    add category page

--- src/templates/category-page/index.test.js
+++ src/templates/category-page/index.test.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { cleanup, render } from '@testing-library/react';
 import { StaticQuery } from 'gatsby';
 import CategoryPage from './index';
-import {
-  createMockGatsbyImageSharpFluid,
-  createMockGroups,
-  createMockPosts,
-} from '../../utils/testing';
+import { createMockGatsbyImageSharpFluid, createMockGroups } from '../../utils/testing';
 import siteMetadata from '../../../gatsby-config';
+import { makeProps } from './mock';
 
-const makeProps = () => ({
-  data: {
-    posts: {
-      edges: createMockPosts,
-    },
-  },
-  pageContext: {
-    limit: 5,
-    skip: 2,
-    numOfPages: 2,
-    numOfPosts: 10,
-    currentPage: 3,
-  },
-});
+afterEach(cleanup);
 
 beforeEach(() => {
-  StaticQuery.mockImplementation(({ render }) => render(
+  StaticQuery.mockImplementation(({ render: renderQuery }) => renderQuery(
     {
       file: createMockGatsbyImageSharpFluid.file,
       site: siteMetadata,
@@ -36,7 +20,23 @@ beforeEach(() => {
 
 describe('CategoryPage', () => {
   it('should render', () => {
-    const tree = renderer.create(<CategoryPage {...makeProps()} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { asFragment } = render(<CategoryPage {...makeProps()} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should have the layout header', () => {
+    const { getByText } = render(<CategoryPage {...makeProps()} />);
+    expect(getByText('Ortmesh')).toBeDefined();
+  });
+
+  it('should have the category as the post listing heading', () => {
+    const { getByText } = render(<CategoryPage {...makeProps()} />);
+    expect(getByText('Self Improvement')).toBeDefined();
+  });
+
+  it('should contain the blog posts', () => {
+    const { getByText } = render(<CategoryPage {...makeProps()} />);
+    expect(getByText('Adding a Project Section to My Website')).toBeDefined();
+    expect(getByText('State Management using React Hooks')).toBeDefined();
   });
 });
---
+++ src/templates/category-page/mock.js
@@ -0,0 +1,17 @@
+import { createMockPosts } from '../../utils/testing';
+
+export const makeProps = () => ({
+  data: {
+    posts: {
+      edges: createMockPosts,
+    },
+  },
+  pageContext: {
+    limit: 5,
+    skip: 2,
+    numOfPages: 2,
+    numOfPosts: 10,
+    currentPage: 3,
+    category: 'Self Improvement',
+  },
+});
