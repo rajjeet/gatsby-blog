e3a1061aa5e8f0bcc15bbce8c58e5a7ec6751b42
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-09-11 07:01:22 -0400

    rename stories to typescript extension

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,7 +5,7 @@ import { withKnobs } from '@storybook/addon-knobs';
 
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /stories\.js$/);
+const req = require.context('../src', true, /stories\.(js|tsx)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
