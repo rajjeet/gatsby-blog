eab39923402583685701a9042f07190c442d0b63
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-09-19 07:10:28 -0400

    move aws deployment code to scripts

---
+++ scripts/aws-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install --user awscli
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set region "$AWS_REGION"
+aws s3 sync public/ s3://$AWS_S3_BUCKET_NAME --acl public-read --delete --exclude CloudFrontLogs*
+aws cloudfront create-invalidation --distribution $AWS_CLOUDFRONT_DISTRIBUTION_ID --paths '/*'
