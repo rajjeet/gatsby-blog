60c1249369ba7f0d5926fd7645e26d5151a07da5
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-09-28 15:40:53 -0400

    add version number to footer (#13)

--- src/components/layout/index.test.tsx
+++ src/components/layout/index.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Layout } from './index';
 import { makeProps } from './mock';
+import { version } from '../../../package.json';
+import { Footer } from './footer';
 
 describe('<Layout />', () => {
   it('should render', () => {
@@ -14,24 +16,33 @@ describe('<Layout />', () => {
     expect(getByText('Sample child')).toBeDefined();
   });
 
-  it('should render an image for website logo', () => {
-    const { getByAltText } = render(<Layout {...makeProps()} />);
-    expect(getByAltText(/ortmesh.*logo/i)).toBeDefined();
+  describe('header', () => {
+    it('should render an image for website logo', () => {
+      const { getByAltText } = render(<Layout {...makeProps()} />);
+      expect(getByAltText(/ortmesh.*logo/i)).toBeDefined();
+    });
+
+    it('should have a link with label home', () => {
+      const { getByLabelText } = render(<Layout {...makeProps()} />);
+      expect(getByLabelText(/home/i)).toBeDefined();
+    });
+
+    it('should have heading called Ortmesh', () => {
+      const { getByText } = render(<Layout {...makeProps()} />);
+      expect(getByText('Ortmesh')).toBeDefined();
+    });
+
+    it('should have a website caption', () => {
+      const { getByText } = render(<Layout {...makeProps()} />);
+      expect(getByText('Write code that matters')).toBeDefined();
+    });
   });
 
-  it('should have a link with label home', () => {
-    const { getByLabelText } = render(<Layout {...makeProps()} />);
-    expect(getByLabelText(/home/i)).toBeDefined();
-  });
-
-  it('should have heading called Ortmesh', () => {
-    const { getByText } = render(<Layout {...makeProps()} />);
-    expect(getByText('Ortmesh')).toBeDefined();
-  });
-
-  it('should have a website caption', () => {
-    const { getByText } = render(<Layout {...makeProps()} />);
-    expect(getByText('Write code that matters')).toBeDefined();
+  describe('footer', () => {
+    it('should have version number in footer', () => {
+      const { getByText } = render(<Footer />);
+      expect(getByText(`v${version}`)).toBeDefined();
+    });
   });
 });
 
