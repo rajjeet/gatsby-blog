d23d5856b3fa5cefd53740e24408376942e3828b
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-10-01 07:04:57 -0400

    remove category listing component

--- src/components/category-listing/stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { CategoryListing } from './index';
-import { makeProps } from './mock';
-
-storiesOf('Category Listing', module)
-  .add('default', () => (
-    <CategoryListing {...makeProps()} />
-  ));
