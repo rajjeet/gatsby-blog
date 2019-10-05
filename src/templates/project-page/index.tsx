45e43fd7dc7c98bc9d441f87868b13b76533c435
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-10-05 06:26:34 -0400

    fix coverage over Anchor tag and fix error with project page not using custom mdx

--- src/templates/project-page/index.tsx
+++ src/templates/project-page/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { Layout } from '../../components/layout';
 import { theme } from '../../utils/theme';
 import { SimplePostListing } from '../../components/simple-post-listing';
 import { SimpleLinkListing } from '../../components/simple-link-listing';
 import { TechStackTagListing } from '../../components/tech-stack-tag-listing';
 import { TProps } from './types';
+import { MarkdownMdxProvider } from '../../utils/markdown-mdx-provider';
 
 const ProjectPage: React.FC<TProps> = ({
   data: {
@@ -24,7 +24,7 @@ const ProjectPage: React.FC<TProps> = ({
       <div>{description}</div>
       {techStackTags && <TechStackTagListing tags={techStackTags} />}
       <S.ContentContainer>
-        <MDXRenderer>{body}</MDXRenderer>
+        <MarkdownMdxProvider content={body} />
         <div>
           {links && <SimpleLinkListing links={links} />}
           {linkedPosts && <SimplePostListing posts={linkedPosts} />}
